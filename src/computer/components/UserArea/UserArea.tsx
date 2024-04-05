67b6511f0099d7a26f4a4f4541014c4091034a3a
Sean Tarzy <seantarzy@Seans-MBP.attlocal.net>
2024-04-05 13:14:03 -0500

    err

--- src/computer/components/UserArea/UserArea.tsx
+++ src/computer/components/UserArea/UserArea.tsx
@@ -25,7 +25,7 @@ function UserArea({
   const isMobile = useIsMobile();
   return (
     <>
-      <div className="absolute bottom-[10%] left-1/4 z-[1000] md:top-2/3  md:left-3/4 md:translate-x-0 tanslate-x-[10px]">
+      <div className="absolute bottom-[2%] left-1/4 z-[1000] md:top-2/3  md:left-3/4 md:translate-x-0 tanslate-x-[10px]">
         {!gameOver && (
           <BaseballButton
             disabled={!btnEnabled}
